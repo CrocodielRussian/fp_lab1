fb099430cbc9a99e04955da1c8980538fcc79581
CrocodielRussian <mk-spbkiselev@yandex.ru>
2025-10-17 12:50:17 +0300

    feat: realise generate/filter task9 0n Haskell

--- src/task9.hs
+++ src/task9.hs
@@ -5,4 +5,17 @@ checkTripleRec' n m
     | (2 * m ^ 2 + 2 * m * n == 1000) = (m ^ 2 - n ^ 2) * 2 * m * n * (m ^ 2 + n ^ 2)
     | otherwise = (checkTripleRec' (n + 1) m)
 
-main = print (checkTripleRec' 0 0)
+
+allVariants' = [(n, m) | n <- [1..25], m <- [1..25]]
+
+targetCondition :: (Int, Int) -> Bool
+targetCondition (n, m) = 2 * m ^ 2 + 2 * m * n == 1000
+
+targetCase = filter targetCondition allVariants'
+
+targetMul :: [(Int, Int)] -> Int
+targetMul [(n, m)] = (m ^ 2 - n ^ 2) * 2 * m * n * (m ^ 2 + n ^ 2)
+
+main = print (targetMul targetCase)
+
+-- main = print (checkTripleRec' 0 0)
