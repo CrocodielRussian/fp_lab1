0a20c811f0ba7eb928d9ad449541066b51e52111
CrocodielRussian <mk-spbkiselev@yandex.ru>
2025-10-24 18:15:34 +0300

    fix: rename test case

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,8 @@
 import Test.HUnit
 import First
 
-check_correctness :: Test
-check_correctness = TestLabel "Check of correct" $ TestList
+check_correctness_task21 :: Test
+check_correctness_task21 = TestLabel "Check of correct task21" $ TestList
   [ TestCase (assertEqual "Recursion version test is not correct" 63252 (amicableNumbersSumRec' 0)),
   TestCase (assertEqual "Tail Recursion version test is not correct" 63252 (amicableNumbersSumRecTail' 0 0)),
   TestCase (assertEqual "Generate/filter/reduce version test is not correct" 63252 (finalSum)),
@@ -13,7 +13,10 @@ check_correctness = TestLabel "Check of correct" $ TestList
 
 tests :: Test
 tests = TestList
-      [ check_correctness ]
+      [ 
+        check_correctness_task21,
+        check_correctness_task9
+      ]
 
 main :: IO Counts
 main = runTestTT tests
